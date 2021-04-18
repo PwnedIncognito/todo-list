ec83124350dcc2c1d604757cd8833d55e7034261
Skayjobra92 <Skayjobra92@users.noreply.github.com>
2021-04-18 21:26:06 +0200

    Added the filter mechanic

--- app.js
+++ app.js
@@ -3,10 +3,12 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
 
 // Event Listeners
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("click", filterTodo);
 
 // Functions
 function addTodo(event) {
@@ -41,7 +43,7 @@ function deleteCheck(event) {
     const todo = item.parentElement;
     // Animation
     todo.classList.add("fall");
-    todo.addEventListener("transitionend", function() {
+    todo.addEventListener("transitionend", function () {
       todo.remove();
     });
   }
@@ -50,4 +52,29 @@ function deleteCheck(event) {
     const todo = item.parentElement;
     todo.classList.toggle("completed");
   }
+}
+
+function filterTodo(event) {
+  const todos = todoList.childNodes;
+  todos.forEach(function (todo) {
+    switch (event.target.value) {
+      case "all":
+        todo.style.display = "flex";
+        break;
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+      case "uncompleted":
+        if (!todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+    }
+  });
 }
